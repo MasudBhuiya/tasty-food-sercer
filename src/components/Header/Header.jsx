ec87f274d1966d7a4fd2ec62f58d53980105a929
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-05-02 16:50:37 +0600

    added active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthProvider';
+import Active from '../Active/Active';
 
 const Header = () => {
   const {user, logOut} = useContext(AuthContext);
@@ -20,16 +21,16 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
+        <li><Active to="/">Home</Active></li>
+        <li><Active to="/blog">Blog</Active></li>
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-xl text-orange-500">Tasty Foods</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/blog">Blog</Link></li>
+    <li><Active to="/">Home</Active></li>
+        <li><Active to="/blog">Blog</Active></li>
     </ul>
   </div>
   <div className="navbar-end">
